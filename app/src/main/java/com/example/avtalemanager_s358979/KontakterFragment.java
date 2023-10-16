a9e0c010c94c9b8d9eb7f8e03bb916ff9d26ce93
KAGrove <knut.a.grove@gmail.com>
2023-10-16 14:11:00 +0200

    Lagring av kontakter i db fungerer

--- app/src/main/java/com/example/avtalemanager_s358979/KontakterFragment.java
+++ app/src/main/java/com/example/avtalemanager_s358979/KontakterFragment.java
@@ -1,5 +1,6 @@
 package com.example.avtalemanager_s358979;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.List;
 
 public class KontakterFragment extends Fragment {
     View view;
@@ -24,7 +27,7 @@ public class KontakterFragment extends Fragment {
         // Initierer elementene
         editTextFornavn = view.findViewById(R.id.fornavn);
         editTextEtternavn = view.findViewById(R.id.etternavn);
-        visalle = view.findViewById(R.id.vis);
+        visalle = view.findViewById(R.id.visalle);
 
         btnLagre = view.findViewById(R.id.lagre);
         btnLagre.setOnClickListener(new View.OnClickListener() {
@@ -47,10 +50,58 @@ public class KontakterFragment extends Fragment {
 
     // Metoder for å lagre og vise kontakt
     private void saveKontakt() {
-        // Implementer koden her for å lagre kontakten
+        final String fornavn = editTextFornavn.getText().toString().trim();
+        final String etternavn = editTextEtternavn.getText().toString().trim();
+
+        class SaveKontakt extends AsyncTask<Void, Void, Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                Kontakt kontakt = new Kontakt();
+                kontakt.setFirstName(fornavn);
+                kontakt.setLastName(etternavn);
+                DatabaseClient.getInstance(getActivity().getApplicationContext())
+                        .getAppDatabase()
+                        .kontaktDao()
+                        .insert(kontakt);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(getActivity().getApplicationContext(), "Saved", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        SaveKontakt st = new SaveKontakt();
+        st.execute();
     }
 
     private void showKontakt() {
-        // Implementer koden her for å vise kontakten
+        class ShowKontakt extends AsyncTask<Void, Void, List<Kontakt>> {
+            @Override
+            protected List<Kontakt> doInBackground(Void... voids) {
+                List<Kontakt> alle = DatabaseClient.getInstance(getActivity().getApplicationContext())
+                        .getAppDatabase()
+                        .kontaktDao()
+                        .getAll();
+                return alle;
+            }
+
+            @Override
+            protected void onPostExecute(List<Kontakt> alle) {
+                super.onPostExecute(alle);
+                Toast.makeText(getActivity().getApplicationContext(), "Vist", Toast.LENGTH_LONG).show();
+                String tekst = "";
+                for (Kontakt bruker : alle) {
+                    tekst += "Fornavn: " + bruker.getFirstName() + " ,Etternavn: " + bruker.getLastName();
+                }
+                visalle.setText(tekst);
+            }
+        }
+
+        ShowKontakt sk = new ShowKontakt();
+        sk.execute();
     }
+
 }
