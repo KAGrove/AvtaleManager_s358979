f93138810ebe4f8d365fa17e2ff955e41b8880b1
KAGrove <knut.a.grove@gmail.com>
2023-10-27 10:51:19 +0200

    Får hentet inn kontakter inn til avtaler

--- app/src/main/java/com/example/avtalemanager_s358979/AvtalerFragment.java
+++ app/src/main/java/com/example/avtalemanager_s358979/AvtalerFragment.java
@@ -1,39 +1,69 @@
 package com.example.avtalemanager_s358979;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AvtalerFragment extends Fragment {
 
     private ListView kontaktListe;
-    private ArrayList<String> kontakter;
-    private ArrayAdapter<String> adapter;
+    private List<Kontakt> kontakter;
+    private ArrayAdapter<Kontakt> adapter;
+
+    private class FetchContactsTask extends AsyncTask<Void, Void, List<Kontakt>> {
+        @Override
+        protected List<Kontakt> doInBackground(Void... voids) {
+            // Initialiser databasen
+            AppDatabase db = DatabaseClient.getInstance(getActivity().getApplicationContext()).getAppDatabase();
+            KontaktDao kontaktDao = db.kontaktDao();
+
+            // Hent kontakter fra databasen
+            return kontaktDao.getAll();
+        }
+
+        @Override
+        protected void onPostExecute(List<Kontakt> fetchedContacts) {
+            kontakter = fetchedContacts;
+
+            // Bruk en egendefinert adapter for å vise kontakter
+            adapter = new ArrayAdapter<Kontakt>(getActivity(), R.layout.list_item_checkedtextview, R.id.checked_text_item, kontakter){
+                @NonNull
+                @Override
+                public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                    View view = super.getView(position, convertView, parent);
+                    TextView text1 = view.findViewById(R.id.checked_text_item);
+                    text1.setText(kontakter.get(position).getFirstName() + " " + kontakter.get(position).getLastName());
+                    return view;
+                }
+            };
+
+            kontaktListe.setAdapter(adapter);
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.avtaler, container, false);
 
         kontaktListe = view.findViewById(R.id.kontaktListe);
-
-        // Fyller listen med eksempelkontakter
-        kontakter = new ArrayList<>();
-        kontakter.add("Kontakt 1");
-        kontakter.add("Kontakt 2");
-        kontakter.add("Kontakt 3");
-
-        adapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_checkedtextview, R.id.checked_text_item, kontakter);
-        kontaktListe.setAdapter(adapter);
+        new FetchContactsTask().execute();
 
         return view;
     }
 }
+
